a6fa7899aea2c4ffe181a68ced2e306a7120431d
Agus Hilman <gushil@gmail.com>
2020-11-23 21:05:22 +0700

    Disable enketo checking on service_health

--- kobo/apps/service_health/views.py
+++ kobo/apps/service_health/views.py
@@ -59,10 +59,10 @@ def service_health(request):
         postgres_message = 'OK'
     postgres_time = time.time() - t0
 
-    t0 = time.time()
-    failure, enketo_message, enketo_content = get_response(settings.ENKETO_INTERNAL_URL)
-    any_failure = True if failure else any_failure
-    enketo_time = time.time() - t0
+    # t0 = time.time()
+    # failure, enketo_message, enketo_content = get_response(settings.ENKETO_INTERNAL_URL)
+    # any_failure = True if failure else any_failure
+    # enketo_time = time.time() - t0
 
     t0 = time.time()
     failure, kobocat_message, kobocat_content = get_response(settings.KOBOCAT_INTERNAL_URL + '/service_health/')
@@ -73,13 +73,13 @@ def service_health(request):
         u'{}\r\n\r\n'
         u'Mongo: {} in {:.3} seconds\r\n'
         u'Postgres: {} in {:.3} seconds\r\n'
-        u'Enketo [{}]: {} in {:.3} seconds\r\n'
+        # u'Enketo [{}]: {} in {:.3} seconds\r\n'
         u'KoBoCAT [{}]: {} in {:.3} seconds\r\n'
     ).format(
         'FAIL' if any_failure else 'OK',
         mongo_message, mongo_time,
         postgres_message, postgres_time,
-        settings.ENKETO_INTERNAL_URL, enketo_message, enketo_time,
+        # settings.ENKETO_INTERNAL_URL, enketo_message, enketo_time,
         settings.KOBOCAT_INTERNAL_URL, kobocat_message, kobocat_time
     )
 
