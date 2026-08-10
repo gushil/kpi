531fbe51871e38930ff2ffdd823624692ff8096b
Agus Hilman <gushil@gmail.com>
2026-08-10 17:30:01 +0700

    OC-28410: Log silent non-social-login skip, per PR review

--- kobo/apps/oc_tenant_auth/signals.py
+++ kobo/apps/oc_tenant_auth/signals.py
@@ -1,7 +1,11 @@
+import logging
+
 from allauth.account.signals import user_logged_in
 from django.dispatch import receiver
 
 from .adapter import sync_login_state
+
+LOGGER = logging.getLogger(__name__)
 
 
 @receiver(user_logged_in, dispatch_uid='oc_tenant_auth.sync_login_state')
@@ -11,5 +15,8 @@ def refresh_login_state(sender, request, user, sociallogin=None, **kwargs):
     Skips non-social logins, which have no sociallogin in signal_kwargs.
     """
     if sociallogin is None:
+        LOGGER.debug(
+            'Skipping sync_login_state for user %s: non-social login', user.pk,
+        )
         return
     sync_login_state(request, user, sociallogin)
